6841a87420f832d625da695e3ab66f91d087fe01
yuhao <yuhao@iwisedev.com>
2018-03-09 10:52:08 +0800

    一些调整

--- src/components/Avatar.vue
+++ src/components/Avatar.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    666
+  <div class="avatar-wrapper">
+    <img src="../assets/logo.png" width="52" height="52">
   </div>
 </template>
 
@@ -9,4 +9,15 @@ export default {
   name:"avatar"
 }
 </script>
+
+<style lang="scss" scoped>
+.avatar-wrapper {
+  width: 52px;
+  height: 52px;
+  border-radius: 50%;
+  overflow: hidden;
+  margin-top: 4px;
+}
+</style>
+
 
--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="profile-wrapper">
     <Avatar></Avatar>
+    <el-popover
+      ref="profile"
+      placement="bottom"
+      width="200"
+      trigger="click"
+      >
+      <div style="text-align:center;">
+        <el-button size="mini" type="text" @click="logout()">退出登录</el-button>
+      </div>
+    </el-popover>
+    <p v-popover:profile>{{username}}</p>
   </div>
 </template>
 
@@ -8,13 +19,32 @@
   import Avatar from '@/components/Avatar'
   export default {
     name:'profile',
-    components:{ Avatar }
+    components:{ Avatar },
+    data(){
+      return {
+        username:'用户9527',
+        visible2:false,
+      }
+    },
+    methods:{
+      logout(){
+
+      }
+    }
   }
 </script>
 
 <style lang="scss" scoped>
 .profile-wrapper {
   display: flex;
-}
+  p {
+    margin: 0 20px 0;
+    cursor: pointer;
+    transition: .3s;
+    &:hover {
+      color: #999;
+    }
+  }
+} 
 </style>
 
