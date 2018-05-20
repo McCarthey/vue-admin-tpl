f0d05e461bc736afdba9f6ad992023928c3da426
mccarthey <mccarthey123@outlook.com>
2018-05-21 00:24:04 +0800

    尝试使用CDN 而不是把vue element打包进

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -4,11 +4,9 @@ const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
 
-function resolve (dir) {
+function resolve(dir) {
   return path.join(__dirname, '..', dir)
 }
-
-
 
 module.exports = {
   context: path.resolve(__dirname, '../'),
@@ -23,10 +21,12 @@ module.exports = {
       : config.dev.assetsPublicPath
   },
   resolve: {
-    extensions: ['.js', '.vue', '.json'],
+    extensions: [
+      '.js', '.vue', '.json'
+    ],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
+      '@': resolve('src')
     }
   },
   module: {
@@ -35,33 +35,28 @@ module.exports = {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: vueLoaderConfig
-      },
-      {
+      }, {
         test: /\.scss$/,
         loaders: ["style-loader", "css-loader", "sass-loader"]
-      },
-      {
+      }, {
         test: /\.js$/,
         loader: 'babel-loader',
         include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
-      },
-      {
+      }, {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
           limit: 10000,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
-      },
-      {
+      }, {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
         loader: 'url-loader',
         options: {
           limit: 10000,
           name: utils.assetsPath('media/[name].[hash:7].[ext]')
         }
-      },
-      {
+      }, {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
         options: {
@@ -75,12 +70,21 @@ module.exports = {
     // prevent webpack from injecting useless setImmediate polyfill because Vue
     // source contains it (although only uses it if it's native).
     setImmediate: false,
-    // prevent webpack from injecting mocks to Node native modules
-    // that does not make sense for the client
+    // prevent webpack from injecting mocks to Node native modules that does not
+    // make sense for the client
     dgram: 'empty',
     fs: 'empty',
     net: 'empty',
     tls: 'empty',
     child_process: 'empty'
+  },
+  /**
+  * 按照官方文档的解释，如果我们想引用一个库，但是又不想让webpack打包，
+  * 并且又不影响我们在程序中以CMD、AMD或者window / global全局等方式进行使用，那就可以通过配置externals。
+  * 这个功能主要是用在创建一个库的时候用的，但是也可以在我们项目开发中充分使用。
+  */
+  externals: {
+    vue: 'Vue',
+    'element-ui': 'elementUI'
   }
 }
